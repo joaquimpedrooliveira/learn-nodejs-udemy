cf7f3f3f7c57e6b6d4c5a9fc0dfc30e01aa4e063
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2018-06-18 15:02:08 -0300

    Adapting todo app to run on Heroku

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+const dbUrl = process.env.
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };

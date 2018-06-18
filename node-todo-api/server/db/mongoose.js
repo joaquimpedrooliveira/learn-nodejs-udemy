b057c111dd2c85640eaeca504289a631e1c86c81
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2018-06-18 15:05:37 -0300

    Correcting bug on db connect

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-const dbUrl = process.env.
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };

a471e7953f3798d6301527d72487723f83daec6f
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2018-06-19 15:48:03 -0300

    Add POST /users/login route

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function(token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return this.findOne({email: email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        //bcrypt does not support promises, so wraps the call around a promise
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (error, success) => {
+                if (success) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+}
+
 UserSchema.pre('save', function(next) {
     var user = this;
 

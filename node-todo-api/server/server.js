dcc8715ea11ebf256c9da0befbb412e98ed207a3
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2018-06-19 09:19:46 -0300

    Setup separate test and dev envs.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,4 @@
+const config = require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -7,8 +8,8 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
+const port = process.env.PORT;
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 

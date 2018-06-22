60233c3289b5fb944a06585854ed709cbcd1d6b1
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2018-06-22 08:41:15 -0300

    Greet new user and alert others

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', { 
-        from: 'Browser',
-        text: 'This is my message',
-        createdAt: new Date().toTimeString()
-    });
 });
 
 socket.on('disconnect', function() {

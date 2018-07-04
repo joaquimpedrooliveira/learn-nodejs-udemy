1fde9a02cf59de6f597ffa13b67a5c966400a5c6
Joaquim Oliveira <joaquim-pedro.oliveira@serpro.gov.br>
2018-07-04 11:14:19 -0300

    Createad currency converter API

---
+++ async-await/currency-convert.js
@@ -0,0 +1,51 @@
+//http://data.fixer.io/api/latest?access_key=d75f8fb3fd0bd5721429a00d6408fe6d
+
+const axios = require('axios');
+
+// Using promises
+// const getExchangeRate = (from, to) => {
+//     return axios.get('http://data.fixer.io/api/latest?access_key=d75f8fb3fd0bd5721429a00d6408fe6d')
+//         .then((response) => {
+//             const euro = 1 / response.data.rates[from];
+//             const rate = euro * response.data.rates[to];
+//             return rate;
+//         });
+// };
+
+const getExchangeRate = async (from, to) => {
+    const response = await axios.get('http://data.fixer.io/api/latest?access_key=d75f8fb3fd0bd5721429a00d6408fe6d')
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+    return rate;
+};
+
+// Using promises
+// const getCountries = (currencyCode) => {
+//     return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+//         .then ((response) => {
+//             return response.data.map((country) => country.name);
+//         });
+// }
+
+const getCountries = async (currencyCode) => {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+    return response.data.map((country) => country.name);
+}
+
+
+const convertCurrency = async (from, to, amount) => {
+    const currencyRate = await getExchangeRate(from, to);
+    const convertedAmount = (amount * currencyRate).toFixed(2);
+    const countries = await getCountries(to);
+    console.log(`${amount} ${from} is worth ${convertedAmount} ${to}. You can spend these in the following countries: ${countries}`);
+}
+
+// getExchangeRate('USD', 'CAD').then((rate) => {
+//     console.log(rate);
+// });
+
+// getCountries('USD').then((countries) => {
+//     console.log(countries)
+// });
+
+convertCurrency('CAD', 'USD', 20);
